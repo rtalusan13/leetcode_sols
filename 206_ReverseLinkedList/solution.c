bb24a932a6c1f3f08438b149365436db2782b088
Raphael Talusan <144483952+rtalusan13@users.noreply.github.com>
2025-11-24 11:24:54 -0600

    Update solution.c

--- 206_ReverseLinkedList/solution.c
+++ 206_ReverseLinkedList/solution.c
@@ -5,6 +5,17 @@
  *     struct ListNode *next;
  * };
  */
+
+
+/*
+Logic breakdown:
+This code catches two main base cases - linked lists of size 0 & 1.
+
+The body runs with 3 pointers, essentially a curr, left, and right based on the current node you're working with.
+The right ptr (save) has no function other than saving the position of the next node to work with, but all logic
+of reversing the list is with saving the left ptr's location and inputting that value into curr->next. There is
+another possible seg fault in this code that can arise in the while loop, but that is caught by the if statement.
+*/
 struct ListNode* reverseList(struct ListNode* head) {
     if(head == NULL){
         return head;
