72dd13524f9c838f60566292298d103a61c2de2f
Raphael Talusan <144483952+rtalusan13@users.noreply.github.com>
2025-11-24 11:35:51 -0600

    Update unoptimal.py

--- 1_TwoSum/unoptimal.py
+++ 1_TwoSum/unoptimal.py
@@ -1,3 +1,6 @@
+"""
+This is actually my first leetcode solution, where I wanted to test out my memory of Python syntax. NOT an optimal solution (which requires a HashMap I believe).
+"""
 class Solution(object):
     def twoSum(self, nums, target):
         for i in range(0,len(nums)):
