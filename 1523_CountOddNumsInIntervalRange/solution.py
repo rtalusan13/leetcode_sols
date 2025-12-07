6e9a8beef4389b752c8dd077b0d10556ea3d554b
Raphael Talusan <144483952+rtalusan13@users.noreply.github.com>
2025-12-07 13:01:05 -0600

    Create solution.py

---
+++ 1523_CountOddNumsInIntervalRange/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def countOdds(self, low, high):
+        return (high+1)/2 - low/2
+        """
+        :type low: int
+        :type high: int
+        :rtype: int
+        """
+        
