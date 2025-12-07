a36f02eb3c8091faa7edb311a1bc09faac537389
Raphael Talusan <144483952+rtalusan13@users.noreply.github.com>
2025-12-07 13:35:58 -0600

    Create solution.java

---
+++ 2980_CheckBitwiseORTrailingZeroes/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean hasTrailingZeros(int[] nums) {
+        int ORbin = 0;
+        for(int i = 0; i < nums.length; i++){
+            if(nums[i] % 2 == 0){
+                ORbin++;
+            }
+            if(ORbin == 2){
+                return true;
+            }
+        }
+        return false;
+    }
+}
