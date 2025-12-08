4e45102aa4135760e6e9241828534eb4ba16deb4
Raphael Talusan <144483952+rtalusan13@users.noreply.github.com>
2025-12-08 01:28:51 -0600

    Create solution.c

---
+++ 3432_CountPartitionsWithEvenSumDifference/solution.c
@@ -0,0 +1,17 @@
+int countPartitions(int* nums, int numsSize) {
+    int* ptr = nums;
+    int rightsum = 0;
+    int leftsum = 0;
+    int partitions = 0;
+    for(int i = 0; i < numsSize; i++){
+        rightsum += nums[i];
+    }
+    for(int i = 0; i < numsSize - 1; i++){
+        leftsum += nums[i];
+        rightsum -= nums[i];
+        if((leftsum - rightsum) % 2 == 0){
+            partitions++;
+        }
+    }
+    return partitions;
+}
