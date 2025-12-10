4bfc68db1b3a41b7a31c8924f1035eda3240f93d
Raphael Talusan <144483952+rtalusan13@users.noreply.github.com>
2025-12-10 16:09:37 -0600

    Create solution.cpp

---
+++ 1518_WaterBottles/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int numWaterBottles(int numBottles, int numExchange) {
+        int sum = numBottles;
+        int rem = 0;
+        while(numBottles > 0){
+            numBottles += rem;
+            rem = numBottles%numExchange;
+            numBottles /= numExchange;
+            sum += numBottles;
+        }
+        return sum;
+    }
+};
